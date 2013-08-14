555ba18d1214614e5b591e751a9247212af489ac
Risa Batta <rbatta@gmail.com>
2013-08-14 11:54:47 -0400

    added more specs to test model

--- spec/controllers/movie_controllers_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'MovieController' do
-  describe 'loading rotten potatoes' do
-  	it 'should load all movies' do
-  		get :index
-  	end
-  end
-end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -6,4 +6,13 @@ describe Movie do
 			Movie.all_ratings.should == ["G", "PG", "PG-13", "NC-17", "R"]
 		end
 	end
+
+	describe 'find by director method' do 
+		it 'should return something' do
+			Movie.find_by_director(1).should eql(1)
+		end
+
+		it 'should return list of movies by similar directors'
+		it 'should not return a list of movies by other directors'
+	end
 end
